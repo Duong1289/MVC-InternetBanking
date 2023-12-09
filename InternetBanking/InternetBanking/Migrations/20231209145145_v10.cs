042eb197934cb368dae94b77103c722dfb872db4
Van Thieu <thieudvts2208034@fpt.edu.vn>
2023-12-09 22:04:09 +0700

    update help request
    update help request

---
+++ InternetBanking/InternetBanking/Migrations/20231209145145_v10.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetBanking.Migrations
+{
+    /// <inheritdoc />
+    public partial class v10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Validation",
+                table: "Transactions",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Transactions",
+                newName: "Validation");
+        }
+    }
+}
