97e21948792d03d681f1b869ff77533a9c339c1e
Van Thieu <thieudvts2208034@fpt.edu.vn>
2023-12-12 23:50:47 +0700

    Fix sendmail help request

--- InternetBanking/InternetBanking/Service/MailService/SendMailService.cs
+++ InternetBanking/InternetBanking/Service/MailService/SendMailService.cs
@@ -1,6 +1,8 @@
-﻿using InternetBanking.Models;
+﻿using InternetBanking.Areas.Identity.Data;
+using InternetBanking.Models;
 using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using MimeKit;
 
@@ -17,10 +19,12 @@ namespace InternetBanking.Service.MailService
     public class SendMailService
     {
         readonly MailSetting MailSetting;
+        private readonly UserManager<InternetBankingUser> _userManager;
 
-        public SendMailService(IOptions<MailSetting> MailSetting)
+        public SendMailService(IOptions<MailSetting> MailSetting, UserManager<InternetBankingUser> _userManager)
         {
             this.MailSetting = MailSetting.Value;
+            this._userManager = _userManager;
         }
         public string GetEmailBody(Transaction transac)
         {
@@ -82,64 +86,65 @@ namespace InternetBanking.Service.MailService
             }
         }
         
-        //public string GetEmailHelpBody(HelpRequest helpRequest)
-        //{
-        //    string templatePath = "Service/MailService/MailProcessRequest.html";
-        //    string template = File.ReadAllText(templatePath);
+        public string GetEmailHelpBody(HelpRequest helpRequest)
+        {
+            string templatePath = "Service/MailService/MailProcessRequest.html";
+            string template = File.ReadAllText(templatePath);
 
-        //    // Replace placeholders with actual content
-        //    template = template.Replace("{{Id}}", helpRequest.Id.ToString())
-        //        .Replace("{{AccountId}}", helpRequest.AccountId)
-        //        .Replace("{{CustomerPersonalId}}", helpRequest.CustomerPersonalId)
-        //        .Replace("{{EmployeeId}}", helpRequest.EmployeeId)
-        //        .Replace("{{RequestTypeId}}", helpRequest.RequestTypeId.ToString())
-        //        .Replace("{{Content}}", helpRequest.Content)
-        //        .Replace("{{CreatedDate}}", helpRequest.CreatedDate.ToString())
-        //        .Replace("{{Answer}}", helpRequest.Answer)
-        //        .Replace("{{Status}}", helpRequest.Status.ToString());
-        //    return template;
-        //}
+            // Replace placeholders with actual content
+            template = template.Replace("{{Id}}", helpRequest.Id.ToString())
+                .Replace("{{AccountId}}", helpRequest.AccountId)
+                .Replace("{{CustomerPersonalId}}", helpRequest.CustomerId)
+                .Replace("{{EmployeeId}}", helpRequest.EmployeeId)
+                .Replace("{{RequestTypeId}}", helpRequest.RequestTypeId.ToString())
+                .Replace("{{Content}}", helpRequest.Content)
+                .Replace("{{CreatedDate}}", helpRequest.CreatedDate.ToString())
+                .Replace("{{Answer}}", helpRequest.Answer)
+                .Replace("{{Status}}", helpRequest.Status.ToString());
+            return template;
+        }
         
-        //public async Task SendEmailHelpRequest(HelpRequest helpRequest) {
-        //    var message = new MimeMessage();
-        //    message.From.Add(new MailboxAddress(MailSetting.DisplayName, MailSetting.Mail));
-        //    message.To.Add(new MailboxAddress("Thieu", "thieu.dv156@gmail.com"));
-        //    message.Subject = "NexBank's Process HelpRequest! Thank you for using our service";
+        public async Task SendEmailHelpRequest(string? id, HelpRequest helpRequest) {
+            var message = new MimeMessage();
+            var customer = await _userManager.FindByIdAsync(id);
+            message.From.Add(new MailboxAddress(MailSetting.DisplayName, MailSetting.Mail));
+            message.To.Add(new MailboxAddress(customer.LastName + customer.LastName, customer.Email));
+            message.Subject = "NexBank's Process HelpRequest! Thank you for using our service";
 
-        //    string htmlBody = GetEmailHelpBody(helpRequest);
+            string htmlBody = GetEmailHelpBody(helpRequest);
 
-        //    var bodyBuilder = new BodyBuilder();
-        //    bodyBuilder.HtmlBody = htmlBody;
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = htmlBody;
 
-        //    message.Body = bodyBuilder.ToMessageBody();
+            message.Body = bodyBuilder.ToMessageBody();
 
-        //    using var client = new SmtpClient();
+            using var client = new SmtpClient();
 
-        //    try
-        //    {
-        //        await client.ConnectAsync(MailSetting.Host, MailSetting.Port, SecureSocketOptions.StartTls);
-        //        await client.AuthenticateAsync(MailSetting.Mail, MailSetting.Password);
-        //        await client.SendAsync(message);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Log the exception or handle it appropriately
-        //        Console.WriteLine($"Error sending email: {ex.Message}");
+            try
+            {
+                await client.ConnectAsync(MailSetting.Host, MailSetting.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(MailSetting.Mail, MailSetting.Password);
+                await client.SendAsync(message);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                Console.WriteLine($"Error sending email: {ex.Message}");
 
-        //        // Save the email to a file for manual inspection (if needed)
-        //        if (!Directory.Exists("sentMail"))
-        //        {
-        //            Directory.CreateDirectory("sentMail");
-        //        }
+                // Save the email to a file for manual inspection (if needed)
+                if (!Directory.Exists("sentMail"))
+                {
+                    Directory.CreateDirectory("sentMail");
+                }
 
-        //        var mailFile = $"sentMail/{Guid.NewGuid()}.eml";
-        //        await message.WriteToAsync(mailFile);
-        //    }
-        //    finally
-        //    {
-        //        await client.DisconnectAsync(true);
-        //    }
-        //}
+                var mailFile = $"sentMail/{Guid.NewGuid()}.eml";
+                await message.WriteToAsync(mailFile);
+            }
+            finally
+            {
+                await client.DisconnectAsync(true);
+            }
+        }
 
     }
 }
