bfeba1eee8f04221d0e2b6d4075c06b8d78bcf29
anhtuan200745 <tuannvats2208021@fpt.edu.vn>
2023-12-14 21:08:29 +0700

    Update Customer.cs

--- InternetBanking/InternetBanking/Models/Customer.cs
+++ InternetBanking/InternetBanking/Models/Customer.cs
@@ -8,54 +8,38 @@ namespace InternetBanking.Models
 {
     public class Customer
     {
-        //CCCD
+        
         [Key]
-        public string? PersonalId { get; set; }
-        [ForeignKey("PersonalId")]
-        public virtual InternetBankingUser? InternetBankingUser { get; set; }
-        // Mối quan hệ 1-1 với IdentityUser
+        public string? Id { get; set; }
 
         [MaxLength(20)]
-        public string? Username { get; set; }
+        public required string? PersonalId { get; set; }
 
-        //[Required]
-        //[MaxLength(20)]
-        //public string? Password { get; set; }
-        [Required]
+        [MaxLength(20)]
+        public required string? Phone {  get; set; }
+
         [MaxLength(50)]
-        public string? FirstName { get; set; }
-        [Required]
+        public required string FirstName { get; set; }
+       
         [MaxLength(50)]
-        public string? LastName { get; set; }
-        [Required]
-        [MaxLength(50)]
-        public string? Email { get; set; }
-        //[Required]
-        //[MaxLength(15)]
-        //public string? Phone { get; set; }
-        [Required]
+        public required string LastName { get; set; }
+       
         [MaxLength(250)]
-        public string? Address { get; set; }
-
-        [Required]
+        public required string Address { get; set; }
+       
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime? OpenDate { get; set; }
+        public required DateTime? OpenDate { get; set; }
 
         //nguoi dung da duoc xac thuc
         [Required]
-        [DefaultValue(false)]
         public bool? Status { get; set; }
 
-        //locked?
-        [Required]
-        [DefaultValue(false)]
-        public bool? Locked { get; set; }
-        [ForeignKey("Branches")]
-        public int? BranchId { get; set; }
         public ICollection<Account>? Accounts { get; set; }
         public ICollection<HelpRequest>? HelpRequests { get; set; }
         public ICollection<Service>? Services { get; set; }
         public ICollection<Image>? Images { get; set; }
-        public ICollection<Loan>? Loans { get; set; }
+        
+        public virtual InternetBankingUser? InternetBankingUser { get; set; }
+        // Mối quan hệ 1-1 với IdentityUser
     }
 }
