e3d986f7fc9155c9d603dedbbe16309cd8f23c2c
anhtuan200745 <tuannvats2208021@fpt.edu.vn>
2023-12-14 20:43:53 +0700

    change login return page

--- InternetBanking/InternetBanking/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ InternetBanking/InternetBanking/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -62,7 +62,7 @@ namespace InternetBanking.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/MyAcount");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
